8fc8d720a0dcfe51f6c2ee7aacd5534b60118215
totaro <66010583+totaro@users.noreply.github.com>
2021-02-04 15:11:40 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
+/* --------------------------------------
+The user interface provides following functionality.
+
+ - List names and amounts, order by amount, most popular first
+ - List names in alphabetical order
+ - The total amount of all the names
+ - Different names
+
+The data for the application is stored in a json file (names.json).
+
+Methods used: sort(), reduce() and lenght()
+
+04022021 - totaro
+---------------------------------------*/
+
 import React, { Component } from 'react'
 import NamesList from './namelist'
 import './App.css'
@@ -6,7 +21,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <h1>dev-academy-2021 exercise - Name Application</h1>
+        <h1>Name Application - reading/sorting/counting json -file</h1>
 		<NamesList />
       </div>
     )
